d24c5dc2ad8f1f5a7e505796577216c97fbc12c4
Ashokkumar T.A <ashokkumar.ta@gmail.com>
2021-07-14 07:14:10 +0530

    configuration updates

--- lean.bat
+++ lean.bat
@@ -2,3 +2,4 @@ call docker-compose -f docker-build/docker-compose.yml run --rm build-container
 call cd  buildx
 call docker buildx bake core
 call docker push ashokkumarta/ids-trusted-connector:latest
+call cd  ..
